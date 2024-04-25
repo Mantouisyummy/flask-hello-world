d81ebe9ce41f95f487b38af5b176cd907d0e5132
Mantouisyummy <51238168+Mantouisyummy@users.noreply.github.com>
2024-04-25 21:19:51 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.get("/")
+async def home():
+    return {"Hello": "World"}
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.get("/chat-exporter")
+async def chat():
+    url = request.query_string.decode("utf-8").replace("url=", "")
+    content = requests.get(url).text
+    return content
+
+app.run(host="0.0.0.0", port=5000)
